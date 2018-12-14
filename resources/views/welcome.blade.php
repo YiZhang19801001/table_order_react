af0371655cd846cc5747dded8a1c641a3211fef4
yizhang19801001 <yizhang19801001@gmail.com>
2018-12-14 17:53:29 +1100

    etc

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,24 +6,16 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <title>{{config('app.name')}}</title>
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
-        <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
-        <link href="https://fonts.googleapis.com/css?family=Raleway:400,500,600,700,800,900" rel="stylesheet">
 
-    <link rel="stylesheet" href="{{asset('css/app.css')}}">
-    <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
+        <link rel="stylesheet" href="{{asset('css/app.css')}}">
 
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons"
-      rel="stylesheet">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.0/animate.css">
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons"
+        rel="stylesheet">
     </head>
 
     <body>
-        <div id="app">
-
-        </div>
-    <script src="{{asset('js/app.js')}}"></script>
+        <div id="app"></div>
+        <script src="{{asset('js/app.js')}}"></script>
 
     </body>
 
